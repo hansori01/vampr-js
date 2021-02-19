4030fc41dbcfdcdd5fbc285db69ed7a840e2b6fa
hansori01 <19sorihan88@gmail.com>
2021-02-19 21:54:06 +0000

    finished the project minus stretch function

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let nodeCount = 0; // counter
+    let currentVamp = this;// current vampire = this
 
+    while (currentVamp.creator) {//while (currentvampire.creator) do loop (if this current vamp has a creator)
+      currentVamp = currentVamp.creator;// change current vampire to currentvampire.creator
+      nodeCount++;// iterate counter
+    }
+
+    return nodeCount;//return counter
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let thisCount = this.numberOfVampiresFromOriginal;
+    const thatCount = vampire.numberOfVampiresFromOriginal
+    return thisCount < thatCount ? true : false;
   }
 
   /** Stretch **/
@@ -39,6 +50,15 @@ class Vampire {
 
   }
 }
+
+
+const vaughn = new Vampire('Vaughn', 2020);
+const sori = new Vampire('Sori', 2021);
+
+vaughn.addOffspring(sori);
+
+console.log(vaughn.isMoreSeniorThan(sori))
+console.log(sori)
 
 module.exports = Vampire;
 
